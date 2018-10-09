f6e920630744b3694379cfdaf17c1ef66cf51b3a
Tyler Buchea <tyler@buchea.com>
2018-10-09 09:45:23 -0700

    Reformat some stuff

--- src/App.js
+++ src/App.js
@@ -1,40 +1,34 @@
-import React, {
-  Component,
-} from 'react';
+import React from 'react';
 
 import { connect } from 'react-redux';
 
-import {
-  activateGeod,
-  closeGeod,
-} from './redux';
+import { activateGeod, closeGeod } from './redux';
 
 // App.js
-export class App extends Component {
-
+export class App extends React.Component {
   render() {
     return (
       <div>
-
         <h1>{this.props.geod.title || 'Hello World!'}</h1>
 
-        {this.props.geod.title ?
-          <button onClick={this.props.closeGeod}>
-            Exit Geod
-          </button> :
-          <button onClick={() => this.props.activateGeod({ title: 'I am a geo dude!' })}>
+        {this.props.geod.title ? (
+          <button onClick={this.props.closeGeod}>Exit Geod</button>
+        ) : (
+          <button
+            onClick={() =>
+              this.props.activateGeod({ title: 'I am a geo dude!' })
+            }
+          >
             Click Me!
           </button>
-       }
-
+        )}
       </div>
     );
   }
-
 }
 
 // AppContainer.js
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = state => ({
   geod: state.geod,
 });
 
